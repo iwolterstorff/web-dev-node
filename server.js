4b3e348766df786d9782ae79d113143ce4da70e6
Ian Wolterstorff <wolterstorff.i@northeastern.edu>
2021-11-20 20:34:37 -0500

    webdev twitter webservice

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+    origin: "*",
+    allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+    ],
+    methods: [
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE",
+        "OPTIONS",
+    ],
+    credentials: true,
+}));
+
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+require("./services/movies-service")(app);
+require("./services/tweeter-service")(app);
+
+app.listen(4000);
